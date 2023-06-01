694fcba9eaae845938abc3885c1347977afeef13
Michael Shaughnessy <michael.shaughnessy@datadoghq.com>
2023-06-01 13:51:08 +0200

    Switch to lazy.nvim

---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      plugins = {
+        presets = {
+          operators = false,
+          motions = false,
+          text_objects = false,
+        },
+      },
+    },
+  },
+}
